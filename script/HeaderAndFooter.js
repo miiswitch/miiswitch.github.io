1ad27c31190f4af6c9b3170f2766098b01b66c88
Antonin <a9laclau@enib.fr>
2024-04-21 12:23:10 -0400

    final change to header

--- script/HeaderAndFooter.js
+++ script/HeaderAndFooter.js
@@ -9,8 +9,8 @@ window.onload = function() {
                 <div id="header-line-container">
                     <a href="/" id="header-left"><h1>Antonin Laclautre</h1></a>
                     <nav id="header-right">
-                        <a href="/projects" class="link-header">Home</a>
-                        <a href="/about" class="link-header">Projects</a>
+                        <a href="/" class="link-header">Home</a>
+                        <a href="/projects" class="link-header">Projects</a>
                         <a href="/contact" class="link-header">Contact</a>
                         <a href="https://www.linkedin.com/in/antonin-laclautre/" class="link-header" id="linkedin-header"><i class="fa-brands fa-linkedin" style="color: #0965c1;"></i></a>
                     </nav>
@@ -18,6 +18,19 @@ window.onload = function() {
             </div>
         `
         document.getElementById("header").appendChild(template.content.cloneNode(true));
+
+        var current_page = document.getElementById("page").getAttribute("data");
+        switch (current_page) {
+            case "Home":
+                document.getElementsByClassName("link-header")[0].classList.add("active");
+                break;
+            case "Projects":
+                document.getElementsByClassName("link-header")[1].classList.add("active");
+                break;
+            case "Contact":
+                document.getElementsByClassName("link-header")[2].classList.add("active");
+                break;
+        }
     }
 
     function addFooter(){
